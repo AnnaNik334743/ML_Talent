af29f5492efba5da183ff637cb536f218717b5f6
AnnaNik334743 <annanikiforova564@gmail.com>
2024-02-29 12:34:15 +0300

    New image storing logic

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import boto3
 import uvicorn
 import tempfile
 from fastapi import FastAPI, UploadFile, File
@@ -7,14 +8,31 @@ from parsing import *
 from json_schemas import Resume
 from openai import OpenAI
 from prompts import ENGLISH_PROMPT, RUSSIAN_PROMPT
-from dotenv import load_dotenv
-import json
 
+from dotenv import load_dotenv
 load_dotenv()
+
+
 API_KEY = os.getenv('API_KEY')
+
 GPT_MODEL = os.getenv('GPT_MODEL')
 CLIENT = OpenAI(api_key=API_KEY)
+
 MODEL = YOLO('yolov8s.pt')
+EXTRACTED_IMG_FOLDER = os.getenv('EXTRACTED_IMG_FOLDER')
+
+BUCKET_HOST = os.getenv('BUCKET_HOST')
+BUCKET_KEY_ID = os.getenv('BUCKET_KEY_ID')
+BUCKET_KEY = os.getenv('BUCKET_KEY')
+BUCKET_SERVICE = os.getenv('BUCKET_SERVICE')
+BUCKET_NAME = os.getenv('BUCKET_NAME')
+
+S3 = boto3.session.Session().client(
+        service_name=BUCKET_SERVICE,
+        endpoint_url=BUCKET_HOST,
+        aws_access_key_id=BUCKET_KEY_ID,
+        aws_secret_access_key=BUCKET_KEY
+)
 
 app = FastAPI()
 
@@ -54,7 +72,7 @@ async def extract_text(file: UploadFile = File(...)):
     file_path = temp_file.name
 
     # Parse the file
-    data = parse(file_path, file_extension, MODEL)
+    data = parse(file_path, file_extension)
 
     # Close and remove the temporary file
     temp_file.close()
@@ -74,8 +92,6 @@ async def parse_text_with_llm(text: str, prompt_language: str):
     Returns:
         Resume: The parsed resume object.
     """
-    global GPT_MODEL, RUSSIAN_PROMPT, ENGLISH_PROMPT
-
     if prompt_language == 'ru':
         d = {"messages": [{"role": "system", "content": f"{RUSSIAN_PROMPT}"},
                           {"role": "user", "content": f"{text}"}]}
@@ -83,15 +99,12 @@ async def parse_text_with_llm(text: str, prompt_language: str):
         d = {"messages": [{"role": "system", "content": f"{ENGLISH_PROMPT}"},
                           {"role": "user", "content": f"{text}"}]}
 
-    # response = CLIENT.chat.completions.create(
-    #     model=GPT_MODEL,
-    #     messages=d['messages']
-    # )
-    #
-    # content = response.choices[0].message.content
+    response = CLIENT.chat.completions.create(
+        model=GPT_MODEL,
+        messages=d['messages']
+    )
 
-    content = "{'resume': {'resume_id': '', 'first_name': 'Антон', 'last_name': 'Бармин', 'middle_name': 'Сергеевич', 'birth_date': '1995-06-14', 'birth_date_year_only': 'false', 'country': 'Россия', 'city': 'Белгород', 'about': 'Experienced Python developer with a focus on ML, DL, Computer Vision, and System Design. Currently exploring mathematics and mathematical modeling.', 'key_skills': ['Python', 'SQL', 'R', 'FastAPI', 'Flask', 'Pytest', 'Postgres', 'Airflow', 'Torch', 'Tensorflow', 'sklearn', 'xgboost', 'pandas', 'numpy', 'statsmodels', 'ggplot2', 'caret', 'Redis', 'GIT', 'Jira', 'Docker', 'Triton-Inference-Server'], 'salary_expectations_amount': '', 'salary_expectations_currency': '', 'photo_path': '', 'language': 'ru', 'gender': 'male', 'resume_name': '', 'source_link': '', 'contactItems': [{'resume_contact_item_id': '', 'value': '89190008691', 'comment': '', 'contact_type': 'Phone'}, {'resume_contact_item_id': '', 'value': 'barmiaa43@mail.ru', 'comment': '', 'contact_type': 'Email'}], 'educationItems': [{'resume_education_item_id': '', 'year': 2017, 'organization': 'БГТУ им. В.Г.Шухова', 'faculty': 'Кафедра “Технической кибернетики”', 'specialty': 'мехатроника и робототехника', 'result': '', 'education_type': 'Basic', 'education_level': 'Bachelor'}, {'resume_education_item_id': '', 'year': 2019, 'organization': 'БГТУ им. В.Г.Шухова', 'faculty': 'Кафедра “Программное обеспечение вычислительной техники и автоматизированных систем', 'specialty': 'информатика и вычислительная техника', 'result': '', 'education_type': 'Basic', 'education_level': 'Bachelor'}], 'experienceItems': [{'resume_experience_item_id': '', 'starts': 2020, 'ends': '', 'employer': 'ООО “Наполеон АЙТИ', 'city': '', 'url': '', 'position': 'Senior Python Developer', 'description': 'ML System Design, Backend development, Model training and deployment.', 'order': 3}, {'resume_experience_item_id': '', 'starts': 2022, 'ends': 2024, 'employer': 'AI Talent Hub', 'city': '', 'url': '', 'position': 'Mentor', 'description': 'Mentoring students on Deep Learning course.', 'order': 2}, {'resume_experience_item_id': '', 'starts': 2018, 'ends': 2020, 'employer': 'ООО “Фабрика информационных технологий”', 'city': '', 'url': '', 'position': 'Python Developer', 'description': 'ML System Design, Backend development, Model training and deployment. Achievements: Recognized for excellence in license plate recognition services.', 'order': 1}], 'languageItems': [{'resume_language_item_id': '', 'language': 'English', 'language_level': 'Intermediate'}]}}"
-    # content = eval(content.replace('\"', '\\"').replace('\"\'', '\\"\"').replace('\'\"', '\'\\"').replace("\'", '\"'))
+    content = response.choices[0].message.content
 
     return eval(content)
 
@@ -115,13 +128,13 @@ async def parse_file(file: UploadFile = File(...)):
     return result
 
 
-@app.get("/download_file")
-async def download_file(file_path: str):
-    """
-    Returns a locally stored file to the user via API.
-    The file_path can be taken from JSON returned by the parse_file or parse_text method.
-    """
-    return FileResponse(file_path, media_type='application/octet-stream', filename='file.jpg')
+# @app.get("/download_file")
+# async def download_file(local_file_path: str):
+#     """
+#     Returns a locally stored file to the user via API.
+#     The file_path can be taken from JSON returned by the parse_file or parse_text method.
+#     """
+#     return FileResponse(local_file_path, media_type='application/octet-stream', filename='file.jpg')
 
 
 if __name__ == "__main__":
